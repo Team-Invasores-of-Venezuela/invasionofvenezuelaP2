357a3570f0c73c9e66f00606cca495e707588ff2
Catalina Miño <cmino21@alumnos.utalca.cl>
2024-11-09 17:04:35 -0300

    creación de archivos

---
+++ backend-edunerd/src/main/java/com/example/backend_edunerd/BackendEdunerdApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_edunerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEdunerdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEdunerdApplication.class, args);
+	}
+
+}
