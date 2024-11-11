e4b9423fb092c936d78e489fdcce6c7eea598349
Yanko <128263461+Yankito@users.noreply.github.com>
2024-11-11 00:53:01 -0300

    Update app.component.ts

--- frontend-edunerd/src/app/app.component.ts
+++ frontend-edunerd/src/app/app.component.ts
@@ -5,11 +5,12 @@ import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import {AdministradorComponent} from'./administrador/administrador.component'
 import {DocenteAdminComponent} from './docente-admin/docente-admin.component';
+import {LoginComponent} from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterModule,HttpClientModule],
+  imports: [RouterOutlet,RouterModule,HttpClientModule,LoginComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
