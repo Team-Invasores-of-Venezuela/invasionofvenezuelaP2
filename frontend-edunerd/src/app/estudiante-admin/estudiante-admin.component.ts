b3861f24f609dbec530bb3e78598ce0861dd0a5f
Franco Bessolo <francobessolo3@gmail.com>
2024-11-16 00:55:54 -0300

    ED-46 Mostrar estudiantes con tamaño variable (ngFor)

--- frontend-edunerd/src/app/estudiante-admin/estudiante-admin.component.ts
+++ frontend-edunerd/src/app/estudiante-admin/estudiante-admin.component.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink, RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
-import {CommonModule, NgIf} from '@angular/common';
+import {CommonModule, NgIf, NgOptimizedImage} from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-estudiante-admin',
   standalone: true,
-  imports: [RouterModule, RouterLink, FormsModule, NgIf, CommonModule, HttpClientModule],
+  imports: [RouterModule, RouterLink, FormsModule, NgIf, CommonModule, HttpClientModule, NgOptimizedImage],
   templateUrl: './estudiante-admin.component.html',
   styleUrl: './estudiante-admin.component.css'
 })
 export class EstudianteAdminComponent {
-  estudiantes: any[] = [];
+  estudiantes = [
+    { matricula: '2021407020',
+      nombre: 'Gustavo Torres',
+      cursos: ['Construcción de Software','Redes de Computadores','Responsabilidad social','Ingeniería Comercial'] },
+
+    { matricula: '2021407019',
+      nombre: 'Mapote Palote',
+      cursos:  ['Teoría de sistemas','Modelos discretos'] },
+
+    { matricula: '2020407020',
+      nombre: 'Franco Bessolo',
+      cursos: ['Física General','Fundamentos de Administración','Construcción de Software','Redes de Computadores','Sistemas Operativos','Máquinas Abstractas'] },
+  ];
   constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
-      this.getEstudiantes();
+      //this.getEstudiantes();
       console.log('Estudiantes: ',this.estudiantes);
   }
 
@@ -25,7 +37,7 @@ export class EstudianteAdminComponent {
     this.http.get<any[]>('http://localhost:8080/estudiante/getall')
       .subscribe(
         (data: any[]) => {
-          console.log('Publicaciones cargadas', data);
+          console.log('Estudiantes obtenidos', data);
 
           this.estudiantes = data;
         },
