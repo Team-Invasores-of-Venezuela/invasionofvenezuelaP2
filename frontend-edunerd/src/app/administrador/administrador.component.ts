70fb9bc73e136f0af33faa2f107cc4c8c1f0270b
ktax06 <prettycaty7@gmail.com>
2024-11-30 15:02:40 -0300

    ED-101 Actualización en la vista principal del adiministrador
    Se mejoró la vista principal del administrador y se agregó un botón para cambiar de modo

--- frontend-edunerd/src/app/administrador/administrador.component.ts
+++ frontend-edunerd/src/app/administrador/administrador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NgIf, CommonModule } from '@angular/common'; // Asegúrate de importar CommonModule
@@ -11,7 +11,7 @@ import {Router, RouterLink} from '@angular/router';
   templateUrl: './administrador.component.html',
   styleUrl: './administrador.component.css'
 })
-export class AdministradorComponent {
+export class AdministradorComponent implements OnInit{
   mostrarFormulario = false;
 
   adminData = {
@@ -21,6 +21,39 @@ export class AdministradorComponent {
     rut: ''
   };
   backendUrl = 'http://localhost:8080/usuario/registeradmin';
+
+  ngOnInit() {
+    this.cargarTema();
+  }
+  claro = false;
+
+  modoOscuro(): void {
+    this.claro = !this.claro;
+    this.actualizarTema();
+  }
+
+  private cargarTema(): void {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      this.claro = true;
+      document.documentElement.classList.add('dark');
+    } else {
+      this.claro = false;
+      document.documentElement.classList.remove('dark');
+    }
+  }
+
+  private actualizarTema(): void {
+    const htmlElement = document.documentElement;
+    if (this.claro) {
+      htmlElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      htmlElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }
+
   registrarAdmin() {
     this.http.post(this.backendUrl, this.adminData).subscribe(
       (response) => {
