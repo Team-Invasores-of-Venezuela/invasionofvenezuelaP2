d2e318193b81ae0d3d18644ad9794425380b983a
Gustavo Torres <gustatorres132@gmail.com>
2024-12-01 01:59:18 -0300

    [ED-96,ED-100] retroalimentacion de intento de login y mostrar datos
    se agrego un texto en rojo que avisa si hubo un error en el inicio de sesion, y tambien, que cuando un admin ponga el mouse en su nombre, aparecen datos de su usuario

--- backend-edunerd/src/main/java/com/example/backend_edunerd/Controladores/ControladorUsuario.java
+++ backend-edunerd/src/main/java/com/example/backend_edunerd/Controladores/ControladorUsuario.java
@@ -38,6 +38,8 @@ public class ControladorUsuario {
             response.put("id", usuario.get().getId());
             response.put("admin", usuario.get().isAdmin());
             response.put("nombre", usuario.get().getRut());
+            response.put("email", usuario.get().getEmail());
+            response.put("imagen",usuario.get().getImageurl());
 
             return ResponseEntity.ok(response);
         } else {
